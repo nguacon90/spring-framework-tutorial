af02bc501d1dbc130ebebeb6e0ecaf61acb9a4ce
minhtq <minhtoquang.it@gmail.com>
2016-06-22 20:20:49 +0700

    update jsf-tutorial

--- jsf-tutorial/src/main/java/vn/com/nguacon/controller/LanguageController.java
+++ jsf-tutorial/src/main/java/vn/com/nguacon/controller/LanguageController.java
@@ -3,16 +3,17 @@ package vn.com.nguacon.controller;
 import java.io.Serializable;
 import java.util.Locale;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "languageBean")
-@SessionScoped
+@ApplicationScoped
 public class LanguageController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	private Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+	private static Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
 
 	public Locale getLocale() {
 		return locale;
@@ -23,6 +24,7 @@ public class LanguageController implements Serializable {
 	}
 
 	public void change(String language) {
+		System.out.println(language);
 		locale = new Locale(language);
 		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(language));
 	}
