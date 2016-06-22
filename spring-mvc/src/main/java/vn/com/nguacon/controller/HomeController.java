33989c376720f51601650be0a7e00293c750bdb3
minhtq <minhtoquang.it@gmail.com>
2016-06-22 21:54:30 +0700

    update spring mvc

--- spring-mvc/src/main/java/vn/com/nguacon/controller/HomeController.java
+++ spring-mvc/src/main/java/vn/com/nguacon/controller/HomeController.java
@@ -1,13 +1,16 @@
 package vn.com.nguacon.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private Logger log = Logger.getLogger(HomeController.class);
 	
 	@RequestMapping("/home")
 	public String openHomePage() {
+		log.info("Load Home Page");
 		return "home";
 	}
 }
